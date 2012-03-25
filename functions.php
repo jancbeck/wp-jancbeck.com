d14e901113ae32b17584d49341e18259c5cadc3d
jancbeck <m@jancb.de>
2012-03-25 19:58:21 +0200

    added project taxonomy

--- functions.php
+++ functions.php
@@ -121,6 +121,33 @@ function register_customs() {
 		'supports' => array( 'title', 'custom-fields', 'thumbnail', 'excerpt' )
 	); 
 	register_post_type('project', $args);
+	
+	$labels = array(
+		'name' => _x( 'Project Type', 'taxonomy general name' ),
+		'singular_name' => _x( 'Project Type', 'taxonomy singular name' ),
+		'search_items' =>  __( 'Search Project Types' ),
+		'popular_items' => __( 'Popular Project Types' ),
+		'all_items' => __( 'All Project Types' ),
+		'parent_item' => null,
+		'parent_item_colon' => null,
+		'edit_item' => __( 'Edit Project Type' ), 
+		'update_item' => __( 'Update Project Type' ),
+		'add_new_item' => __( 'Add New Project Type' ),
+		'new_item_name' => __( 'New Project Type Name' ),
+		'separate_items_with_commas' => __( 'Separate Project Type with commas' ),
+		'add_or_remove_items' => __( 'Add or remove writers' ),
+		'choose_from_most_used' => __( 'Choose from the most used writers' ),
+		'menu_name' => __( 'Project Type' ),
+	); 
+	
+	register_taxonomy('projects','project',array(
+		'hierarchical' => false,
+		'labels' => $labels,
+		'show_ui' => true,
+		'update_count_callback' => '_update_post_term_count',
+		'query_var' => true,
+		'rewrite' => array( 'slug' => 'projects' ),
+	));
 }
 
 
