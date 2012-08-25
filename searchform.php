5f2289dac77edc69761a263c8a6a488755a72779
jancbeck <delite.jcb@gmail.com>
2012-08-25 18:05:45 +0200

    added bootstrap-style searchform

--- searchform.php
+++ searchform.php
@@ -1,6 +1,3 @@
-<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-    <div><label class="hidden" for="s">Suche nach:</label>
-        <input type="text" value="" name="s" id="s" />
-        <input type="submit" id="searchsubmit" value="Suche starten" />
-    </div>
+<form class="navbar-search pull-right" method="get" action="<?php echo home_url(); ?>">
+	<input type="text" id="s" name="s" class="search-query span3" placeholder="Suche">
 </form>
