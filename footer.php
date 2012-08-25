8f1272f20d4051c75b1909f787b640988fe55358
jancbeck <delite.jcb@gmail.com>
2012-08-26 00:11:10 +0200

    added some info to the footer

--- footer.php
+++ footer.php
@@ -1,4 +1,12 @@
 <?php wp_footer(); ?>
 
+<hr>
+
+<p class="footer">
+	<a href="http://twitter.com/jancbeck">Twitter</a>   
+	<a href="https://github.com/jancbeck/">GitHub</a>
+	<a href="<?php bloginfo('rss2_url'); ?>">Feed</a>
+	<a href="mailto:mail@jancbeck.de">Email</a>
+</p>
 </body>
 </html>
