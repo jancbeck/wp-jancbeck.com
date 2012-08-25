b8b22387ad20f37d982eb4de9a4b14de36f257ba
jancbeck <delite.jcb@gmail.com>
2012-08-25 19:22:55 +0200

    added post-category display

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 <?php if ( have_posts() ) : while (have_posts()) : the_post(); ?>
 		
-		<p><small>PORTFOLIO</small></p>
+		<p class="post-category"><?php echo get_first_category(); ?></p>
 		<h2><a href="<?php echo get_permalink(); ?>"><?php the_title();?></a></h2>
 
 		<div class="post">
