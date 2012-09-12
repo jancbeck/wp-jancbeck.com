e728224ee49fad9f5cca3c7d753c0e588d8f23b5
jancbeck <delite.jcb@gmail.com>
2012-09-13 01:48:48 +0200

    added license info

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
 	<a href="http://twitter.com/jancbeck">Twitter</a>   
 	<a href="https://github.com/jancbeck/">GitHub</a>
 	<a href="<?php bloginfo('rss2_url'); ?>">Feed</a>
-	<br><small>Powered by WordPress</small>
+	<br><small>CC-BY-SA 3.0</small>
 </p>
 </body>
 </html>
