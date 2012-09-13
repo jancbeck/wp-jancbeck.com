2db915574198905aa96d2108f4a7df0494eabd1a
jancbeck <delite.jcb@gmail.com>
2012-09-14 01:53:54 +0200

    refined front page

--- front-page.php
+++ front-page.php
@@ -2,76 +2,72 @@
 
 <div class="container">
 
-<h1 class="page-header">Jan Beck</h1>
-
-<h2><a href="#">Über mich</a></h2>
-
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-
-<h2><a href="#">Portfolio</a></h2>
-
-<?php $projects = get_posts( 'category_name=portfolio' ); 
-	  $i = 0; ?>
-	
-<div id="myCarousel" class="carousel slide">
-  <!-- Carousel items -->
-  <div class="carousel-inner">
-  
-  <?php	foreach( $projects as $project ) : ?>
-    
-    <div class="item<?php echo( $i++ ? '' : ' active'); ?>">
-    	<?php echo get_the_post_thumbnail( $project->ID, 'full' ); ?>
-		<div class="carousel-caption">
-          <h4><?php echo $project->post_title?></h4>
-          <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-        </div>
-	</div>
-    
-  <?php endforeach; ?>
-    
-  </div>
-  <!-- Carousel nav -->
-  <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
-  <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
-</div>
-	
-<h2><a href="#">Blog</a></h2>
-
-<?php if ( have_posts() ) : while (have_posts()) : the_post(); ?>
-
-	<div class="post">
-	
-		<p class="post-date"><?php the_date() ?></p>
-		<h3><a href="<?php echo get_permalink(); ?>"><?php the_title();?></a></h3>
-
-		<div class="post-content">
+	<div class="page-section">
 		
-			<?php the_content( '' ); ?>
-			
-			<?php if ( has_more() ) {
-			
-					if ( has_post_thumbnail() ) {
-						echo '<p><a href="'. get_permalink() .'">'. get_the_post_thumbnail( ). '</a></p>';
-					}
-					echo '<p class="more-link"><a href="'. get_permalink() .'">'. __( 'Continue Reading' ) .'</a>';
-				
-				 } else {
-				 
-					if ( has_post_thumbnail() ) {
-						echo '<p>'. get_the_post_thumbnail( ). '</p>';
-					}
-			 } ?>
-		</div>
-
+		<p class="intro">Hallo, ich bin Webdesigner und -entwickler. Ich mache Webseiten und dies ist meine.</p>
+	
 	</div>
-
-	<?php endwhile;	?>
 	
-	<?php endif; ?>
+	<div class="page-section">
 	
-<h2><a href="#">Contact</a></h2>
+		<h2 class="page-section-title"><a href="#">Projects</a></h2>
+		
+		<?php $projects = get_posts( 'category_name=portfolio' ); 
+			  $i = 0; ?>
+			
+		<div id="projects-carousel" class="carousel slide">
+		  <!-- Carousel items -->
+		  <div class="carousel-inner">
+		  
+		  <?php	foreach( $projects as $project ) : ?>
+		    
+		    <a href="<?php echo get_permalink( $project->ID ); ?>" class="item<?php echo( $i++ ? '' : ' active'); ?>">
+		    	<?php echo get_the_post_thumbnail( $project->ID, 'large', array('class'	=> "attachment-large shadowless") ); ?>
+				<div class="carousel-caption">
+		          <h4><?php echo $project->post_title; ?></h4>
+		        </div>
+			</a>
+		    
+		  <?php endforeach; ?>
+		    
+		  </div>	
+		  <!-- Carousel nav -->
+		  <a class="carousel-control left" href="#projects-carousel" data-slide="prev">&lsaquo;</a>
+		  <a class="carousel-control right" href="#projects-carousel" data-slide="next">&rsaquo;</a>
+		</div>
+	
+	</div>
+	
+	<div class="page-section page-section-blog">
+		
+		<h2 class="page-section-title"><a href="#">Blog</a></h2>
+		
+		<div class="row">
+		
+			<?php 
+			$posts = get_posts( 'numberposts=3' );
+			if ( $posts ) : foreach( $posts as $post ) : setup_postdata($post); ?>
+			
+			<div class="span4">
+			
+				<p class="post-date"><?php the_date() ?></p>
+				<h4><a href="<?php echo get_permalink(); ?>"><?php the_title();?></a></h4>
+		
+			</div>
+	
+			<?php endforeach; ?>
+			<?php endif; ?>
 
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
+	</div>
+	
+	<div class="page-section">
+
+		<h2 class="page-section-title"><a href="#">Contact</a></h2>
+	
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		
+	</div>
 	
 </div>
 
