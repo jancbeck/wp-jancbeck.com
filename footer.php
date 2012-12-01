9be6c0b9bb2e82c3883abb9f2684e31940c5a55e
jancbeck <delite.jcb@gmail.com>
2012-12-01 15:49:35 +0100

    removed old links

--- footer.php
+++ footer.php
@@ -4,7 +4,6 @@
 		'container' => '',
 		'menu_class' => 'navigation'	
 	)); ?>
-<!--	<p><small><a href="http://creativecommons.org/licenses/by-sa/3.0/de/">CC-BY-SA 3.0</a> | <a href="<?php echo get_permalink(get_page_by_title('Impressum')) ?>">Impressum</a></small></p>-->
 </div>
 
 <?php wp_footer(); ?>
