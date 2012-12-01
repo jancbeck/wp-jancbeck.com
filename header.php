6347774a0bdb8755bb8dd6b980489d54b70ee9b4
jancbeck <delite.jcb@gmail.com>
2012-12-01 13:04:27 +0100

    clean up

--- header.php
+++ header.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
 <html lang="<?php bloginfo('language'); ?>" class="no-js">
 <head>
-	<script>(function(H){H.className=H.className.replace(/\bno-js\b/,'js')})(document.documentElement) /* removes no-js class */</script>
 
 	<meta charset="utf-8" />
 	<meta name="author" content="<?php echo get_userdata(1)->nickname; ?>">
-	<meta name="description" content="Jan Beck about the web.">
+	<meta name="description" content="Jan Beck writes about what he learned and thinks about.">
 	<meta name="country" content="DE">
 	<meta name="language" content="<?php bloginfo('language'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -13,20 +12,16 @@
 
 	<link href="http://fonts.googleapis.com/css?family=PT+Sans:400,700,400italic|PT+Mono|PT+Serif:400,700,400italic,700italic" rel="stylesheet">
 
-	<link rel="icon" href="<?php bloginfo('template_url'); ?>/images/favicon.ico" type="image/x-icon">
-	<!--
-	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
-		<link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
-	-->
+	<link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/images/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="<?php bloginfo('template_url'); ?>/images/favicon.png" type="image/png">
+	<link rel="apple-touch-icon" href="<?php bloginfo('template_url'); ?>/images/apple-touch-icon.png"/>
 
 	<?php wp_head(); ?>
 
-	<title><?php wp_title('|', true, 'right'); bloginfo('name'); ?></title>
+	<title><?php wp_title('|', true, 'right'); bloginfo('name'); echo(!is_front_page() ? '' : ' | '. get_bloginfo('description')) ?></title>
 </head>
 
-<body>
+<body <?php body_class(); ?>>
 
 <header role="banner">
 	<h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
