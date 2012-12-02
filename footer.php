badf3788111d706482c015247caa801fc0edb322
jancbeck <delite.jcb@gmail.com>
2012-12-02 03:52:53 +0100

    added google analytics

--- footer.php
+++ footer.php
@@ -4,7 +4,23 @@
 		'container' => '',
 		'menu_class' => 'navigation'	
 	)); ?>
-</div>
+	<a class="scroll-to-top" href="#top">↑</a>
+</footer>
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', <?php echo get_theme_mod('ga_code') ?>]);
+  _gaq.push (['_gat._anonymizeIp']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 
 <?php wp_footer(); ?>
 
