1285386e821ba8f0ffc21e9127d16f28b4f0e31e
jancbeck <delite.jcb@gmail.com>
2012-12-02 03:58:22 +0100

    removed static content

--- header.php
+++ header.php
@@ -4,27 +4,25 @@
 
 	<meta charset="utf-8" />
 	<meta name="author" content="<?php echo get_userdata(1)->nickname; ?>">
-	<meta name="description" content="Jan Beck writes about what he learned and thinks about.">
-	<meta name="country" content="DE">
 	<meta name="language" content="<?php bloginfo('language'); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="generator" content="WordPress" />
 
 	<link href="http://fonts.googleapis.com/css?family=PT+Sans:400,700,400italic|PT+Mono|PT+Serif:400,700,400italic,700italic" rel="stylesheet">
 
-	<link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/images/favicon.ico" type="image/x-icon">
-	<link rel="icon" href="<?php bloginfo('template_url'); ?>/images/favicon.png" type="image/png">
-	<link rel="apple-touch-icon" href="<?php bloginfo('template_url'); ?>/images/apple-touch-icon.png"/>
+	<link rel="shortcut icon" href="<?php echo get_theme_mod('favicon_ico') ?>" type="image/x-icon">
+	<link rel="icon" href="<?php echo get_theme_mod('favicon_png') ?>" type="image/png">
+	<link rel="apple-touch-icon" href="<?php echo get_theme_mod('favicon_apple') ?>"/>
 
 	<?php wp_head(); ?>
 
 	<title><?php wp_title('|', true, 'right'); bloginfo('name'); echo(!is_front_page() ? '' : ' | '. get_bloginfo('description')) ?></title>
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php body_class(); ?> id="top">
 
 <header role="banner">
-	<h1><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></h1>
+	<a href="<?php echo home_url(); ?>"><?php echo get_theme_mod('header_text') ?></a>
 </header>
 <nav role="navigation">
 	<?php wp_nav_menu( array(
